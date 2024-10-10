0a173eacc2e97ba16941de407734473ac2908938
itaksssss <juwitamelinda930@gmail.com>
2024-10-09 20:25:51 -0800

    Add files via upload

---
+++ puisi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <title> Puisi Mata </title>
+</head>
+<body>
+    <h3>
+        Puisi Mata
+    <h3>
+        <p>
+            <br>Mata, lentera hidup yang tak pernah padam,
+            <br>menyimpan cerita, harapan, dan mimpi dalam.
+            <br>Lewat sinarnya, dunia terbuka,
+            <br>menyingkap warna, rasa, hingga lara.
+</p>
+            <p>
+<br>Dengan mata, kita tahu arti senja,  
+<br>dan menangkap rindu di batas cakrawala.  
+<br>Mereka menjadi saksi dalam tiap langkah,  
+<br>mencatat jejak waktu yang terus berubah.
+</p> 
+<p>
+<br>Mata, jendela bagi jiwa yang peka,  
+<br>mengalirkan kasih dan duka yang tak terucap kata.  
+<br>Ia mengerti ketika kata tak sanggup terungkap,  
+<br>dan memberi cahaya saat hati gelap.   
+</p>
+<p>
+<br>O, mata yang kecil namun berharga,  
+<br>penuntun hidup, penjaga asa.  
+<br>Jagalah mereka, si penerima kebenaran,  
+<br>sebab tanpa mata, dunia mungkin kelam dalam keheningan.
+</p>
+
+</body>
+</html>
